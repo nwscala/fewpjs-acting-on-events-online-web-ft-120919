110bd361c15d5c8e73110ec9bdf65a9b86f704bb
Noah Scala <nwscala@gmail.com>
2020-03-26 12:42:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+let dodger = document.getElementById("dodger")
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    let right = left + 40;
+   
+    if (right < 400) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
